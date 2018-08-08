1f936bf3fcb8f99be4022a6dd2a693a2c982fd57
Axel281296 <39245301+Axel281296@users.noreply.github.com>
2018-08-08 15:30:40 -0500

    arreglo2

---
+++ arreglos2/src/arreglos2/sub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos2;
+
+/**
+ *
+ * @author User
+ */
+public class sub {
+    int [] arreglo = new int[70];
+    public int a;
+    public void sub(){
+    for (int i=0; i>=70; i++){
+        a=1+a;
+        this.arreglo[i]= a;
+      }
+     }
+    
+    
+}
