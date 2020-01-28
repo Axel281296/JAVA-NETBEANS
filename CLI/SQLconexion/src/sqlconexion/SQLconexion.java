50e3f7b3ea567cb6517665e9926317696cee6370
Axel281296 <39245301+Axel281296@users.noreply.github.com>
2020-01-28 11:49:56 -0500

    AXEL SALAZAR

---
+++ CLI/SQLconexion/src/sqlconexion/SQLconexion.java
@@ -0,0 +1,9 @@
+
+package sqlconexion;
+
+public class SQLconexion {
+
+    public static void main(String[] args) {
+        sqlconexion.conexion.getConexion();
+    }
+}
---
+++ CLI/SQLconexion/src/sqlconexion/conexion.java
@@ -0,0 +1,46 @@
+package sqlconexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+
+public class conexion {
+     static Connection contacto = null;
+     public static Connection getConexion(){
+      String url = "jdbc:sqlserver://SERVERCSLF\\SQLEXPRESS:1433;databaseName=LabPro";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            
+        }catch (ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null, "No se pudo establece la conexion... revisar Driver" + e.getMessage(),
+            "Error de Conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        try{
+            contacto = DriverManager.getConnection(url,"sa","1234");//clave y usuario
+        }catch (SQLException e){
+             JOptionPane.showMessageDialog(null, "Error" + e.getMessage(),
+            "Error de Conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        return contacto;
+
+     }
+     
+       public static ResultSet Consulta(String consulta){
+        Connection con = getConexion();
+        Statement declara;
+        try{
+            declara=con.createStatement();
+            ResultSet respuesta = declara.executeQuery(consulta);
+            return respuesta;
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error" + e.getMessage(),
+            "Error de Conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        return null;
+    }
+     
+}
